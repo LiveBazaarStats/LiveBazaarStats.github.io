7db8cbe8027dc37ece0dfb8cd067fba35aec9230
Olle Lukowski <63189113+Olle-Lukowski@users.noreply.github.com>
2022-07-26 16:28:56 +0200

    Updates

---
+++ precache-manifest.9dccab89c2cd27650ca44ff18ed8c745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e84f6e256799ad27a23",
+    "url": "/static/css/main.211906ba.chunk.css"
+  },
+  {
+    "revision": "6e84f6e256799ad27a23",
+    "url": "/static/js/main.6e84f6e2.chunk.js"
+  },
+  {
+    "revision": "f5e8c6976c9155df05df",
+    "url": "/static/js/1.f5e8c697.chunk.js"
+  },
+  {
+    "revision": "359046d8729ced8b8fca",
+    "url": "/static/js/2.359046d8.chunk.js"
+  },
+  {
+    "revision": "7c630830df395a212194",
+    "url": "/static/js/runtime~main.7c630830.js"
+  },
+  {
+    "revision": "5ffde2c1d01fa44fa94463da08fcd1d2",
+    "url": "/index.html"
+  }
+];
