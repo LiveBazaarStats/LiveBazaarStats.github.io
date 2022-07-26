047c45fa4806bb0dce8881eb818ca6034f21396e
Olle Lukowski <63189113+Olle-Lukowski@users.noreply.github.com>
2022-07-26 16:50:47 +0200

    Updates

--- precache-manifest.69e8fcab5c6e69932d7949abda29201c.js
+++ precache-manifest.69e8fcab5c6e69932d7949abda29201c.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "6e84f6e256799ad27a23",
+    "revision": "1100180bf1282271583f",
     "url": "/static/css/main.211906ba.chunk.css"
   },
   {
-    "revision": "6e84f6e256799ad27a23",
-    "url": "/static/js/main.6e84f6e2.chunk.js"
+    "revision": "1100180bf1282271583f",
+    "url": "/static/js/main.1100180b.chunk.js"
   },
   {
     "revision": "f5e8c6976c9155df05df",
     "url": "/static/js/1.f5e8c697.chunk.js"
   },
   {
-    "revision": "359046d8729ced8b8fca",
-    "url": "/static/js/2.359046d8.chunk.js"
+    "revision": "07ced9d895f7b167f4dc",
+    "url": "/static/js/2.07ced9d8.chunk.js"
   },
   {
     "revision": "7c630830df395a212194",
     "url": "/static/js/runtime~main.7c630830.js"
   },
   {
-    "revision": "5ffde2c1d01fa44fa94463da08fcd1d2",
+    "revision": "dcb14c69b36514933a8f428afd248159",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9dccab89c2cd27650ca44ff18ed8c745.js"
+  "/precache-manifest.69e8fcab5c6e69932d7949abda29201c.js"
 );
 
 workbox.clientsClaim();
